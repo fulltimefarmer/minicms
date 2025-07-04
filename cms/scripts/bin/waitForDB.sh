a1771d7b4c33b58dd972f8bd5109ceb2e044526d
Max Zhou <max.zhou@lego.com>
2025-07-04 10:00:22 +0800

    update

--- cms/scripts/bin/waitForDB.sh
+++ cms/scripts/bin/waitForDB.sh
@@ -4,9 +4,10 @@ project_path=$(pwd)
 project_name="${project_path##*/}"
 
 seconds=1
-until docker container exec -it $project_name-postgres-1 psql -U dbuser -d minicommerce -c "select 1" > /dev/null 2>&1; do
+until docker container exec -itd $project_name-postgres-1 psql -U dbuser -d minicms -c "select 1" > /dev/null 2>&1; do
   >&2 echo "Postgres is starting, already wait for $seconds seconds..."
   sleep 1
   seconds=$(expr $seconds + 1)
 done
 >&2 echo "Postgres is started"
+exit 0
